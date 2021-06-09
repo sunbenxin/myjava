dfc159ed0d697925f9c77c52431db4aee159cb5b
sunbenxin <sunbenxin@bitforex.com>
2021-06-09 11:54:28 +0800

    Init code

---
+++ src/main/java/com/sunbx/myjava/MyjavaApplication.java
@@ -0,0 +1,13 @@
+package com.sunbx.myjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyjavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyjavaApplication.class, args);
+    }
+
+}
